d1dd427876045b2301278959440a1e6c23c77702
JamDon2 <hello@jamdon2.dev>
2022-10-06 19:08:15 +0200

    Add stuff to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Button, Toolbar } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 import APILogin from "./APILogin";
@@ -7,9 +8,19 @@ import LoginStatus from "./LoginStatus";
 
 export default function Header() {
     return (
-        <AppBar elevation={0} color="transparent">
+        <AppBar elevation={0} color="inherit" position="sticky">
             <Toolbar className={styles.container}>
-                <div></div>
+                <div className={styles.group}>
+                    <Link href="/">
+                        <span className={styles.title}>Stable Horde</span>
+                    </Link>
+                    <Link href="/workers">
+                        <Button variant="outlined">All workers</Button>
+                    </Link>
+                    <Link href="/workers/saved">
+                        <Button variant="outlined">Your workers</Button>
+                    </Link>
+                </div>
                 <div className={styles.group}>
                     <LoginStatus />
                     <APILogin />
---
+++ pages/workers.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Workers: NextPage = () => {
+    return <></>;
+};
+
+export default Workers;
